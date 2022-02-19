6c9593dc8a22b5dd5fd37b6da950f7c845cc6cd9
vyomg <83865210+vyomg@users.noreply.github.com>
2022-02-19 14:44:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,70 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-
+  
 var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox"); 
 
 function start()
 {
-    document.getElementById("textbox").innerHTML = "";
+    Textbox.innerHTML = ""; 
     recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
 }
 
-recognition.onresult = function (event) {
-
-    console.log(event);
-
-    var Content = event.results [0] [0].transcript;
-    document.getElementById("textbox").innerHTML = Content;
-    console.log(Content) ;
-    if(Content =="take my selfie")
-    {
-    console.log("taking selfie --- ");
-    speak();
-    }
-
-}
 
 function speak(){
     var synth = window.speechSynthesis;
+
     speak_data = "Taking you Selfie in 5 seconds";
+
     var utterThis = new SpeechSynthesisUtterance(speak_data);
+
     synth.speak(utterThis);
+
     Webcam.attach(camera);
 
     setTimeout(function()
-    {
-        take_snapshot();
+    { 
+        take_selfie(); 
         save();
     }, 5000);
 }
 
+ 
+camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
-    image_format = 'png',
-    png_quality:90
+    image_format : 'jpeg',
+    jpeg_quality:90
 });
-camera = document.getElementById("camera");
 
-function take_snapshot()
+function take_selfie()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'" >';
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
+
 function save()
 {
-    link = document.getElementById("link");
-    image = document.getElementById("selfie_image").src ;
-    link_href = image;
-    link.click();
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
 }
